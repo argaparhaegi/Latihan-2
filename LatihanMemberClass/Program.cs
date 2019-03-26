1cbf2932d5786380d42e317ff14116147368e897
Arga <argaparhagy03@gmail.com>
2019-03-26 18:33:02 +0700

    tugas2

---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Reza";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LatihanMemberClass/Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name = {0}", DriverName);
+            Console.WriteLine("On Duty = {0}", OnDuty);
+            Console.WriteLine("Num Passenger = {0}", NumPassenger);
+
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Selesai menjemput Penumpang", DriverName);
+        }
+
+    }
+}
